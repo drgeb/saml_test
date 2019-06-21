b8db574e5b819bf8dedbd0ba29a36fd2db6d4dfc
drgeb <bitbucket@drgeb.com>
2019-06-20 22:23:38 -0500

    Initial commit

---
+++ todo-api/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// tag::code[]
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
+// end::uri-configuration[]
+// end::code[]
+
